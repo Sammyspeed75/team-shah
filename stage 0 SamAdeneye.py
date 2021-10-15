2179b214f2ad692750c437bc32c0998221f067c5
Sammyspeed75 <92327878+Sammyspeed75@users.noreply.github.com>
2021-10-15 11:02:31 +0100

    Add files via upload

--- stage 0 SamAdeneye.py
+++ stage 0 SamAdeneye.py
@@ -1,7 +1,10 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
 
-print ("name = ADENEYE, Samuel Adewale")
-print ("email = samueladeneye75@gmail.com ")
-print ("slack_username = SamAdeneye")
-print ("biostack = Data Science and Machine Learning")
-print ("twitter_username = Unkool_Sam")
-print ("hamming distance = 1")
+= RESTART: C:\Users\USER\AppData\Local\Programs\Python\Python310\stage 0 SamAdeneye.py
+name = ADENEYE, Samuel Adewale
+email = samueladeneye75@gmail.com 
+slack_username = SamAdeneye
+biostack = Data Science and Machine Learning
+twitter_username = Unkool_Sam
+hamming distance = 1
