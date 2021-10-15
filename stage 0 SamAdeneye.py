a86e52cc132749a27ccda37df30c9fcc2379e9d8
Sammyspeed75 <92327878+Sammyspeed75@users.noreply.github.com>
2021-10-15 10:58:27 +0100

    Add files via upload

---
+++ stage 0 SamAdeneye.py
@@ -0,0 +1,8 @@
+
+
+print ("name = ADENEYE, Samuel Adewale")
+print ("email = samueladeneye75@gmail.com ")
+print ("slack_username = SamAdeneye")
+print ("biostack = Data Science and Machine Learning")
+print ("twitter_username = Unkool_Sam")
+print ("hamming distance = 1")
